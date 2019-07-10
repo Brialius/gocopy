9d48025cd7cbb42220ae76a9498caf1a4b1983f2
Denis Belyatsky <denis.bel@gmail.com>
2019-07-10 16:44:56 +0300

    Add tests for gocopy.validate()

--- gocopy/gocopy_test.go
+++ gocopy/gocopy_test.go
@@ -24,3 +24,53 @@ func TestCopy(t *testing.T) {
 		})
 	}
 }
+
+func Test_validate(t *testing.T) {
+	type args struct {
+		limit     int64
+		inputSize int64
+		offset    int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"OK",
+			args{1, 5, 0},
+			false,
+		}, {
+			"Fail empty source",
+			args{1, 0, 0},
+			true,
+		}, {
+			"Fail negative limit",
+			args{-10, 5, 0},
+			true,
+		}, {
+			"Fail zero limit",
+			args{0, 5, 0},
+			true,
+		}, {
+			"Fail negative offset",
+			args{1, 5, -1},
+			true,
+		}, {
+			"Fail limit + offset is greater sorurce size",
+			args{5, 5, 1},
+			true,
+		}, {
+			"OK limit + offset is equal sorurce size",
+			args{5, 6, 1},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := validate(tt.args.limit, tt.args.inputSize, tt.args.offset); (err != nil) != tt.wantErr {
+				t.Errorf("validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
